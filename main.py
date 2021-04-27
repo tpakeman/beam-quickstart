ff176ea1f0b65f1a6a7da721884200c9e00b4cf4
Tom Pakeman <tegpakeman@gmail>
2021-04-27 10:42:37 +0100

    Initial fun

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-#  Copyright 2021 Israel Herraiz
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='A beam pipeline!')
+    parser.add_argument('--input', '-i', help='input textl location')
+    parser.add_argument('--output', '-o', help='input textl location')
+    our_args, dataflow_args = parser.parse_known_args()
+    run_pipeline(our_args, dataflow_args)
+
+
+def format_output(sorted_words):
+    return ' '.join([f"{w},{n}" for w, n in sorted_words])
+
+def run_pipeline(custom_args, runner_args):
+    opts = PipelineOptions(runner_args)
+    infile = custom_args.input
+    outfile = custom_args.output
+
+    with beam.Pipeline(options=opts) as p:
+        # (p.apply(some_func, 'some_label')) == (p | 'some_label' >> some_func)
+        lines = p | 'Read input text' >> beam.io.ReadFromText(infile)
+        # lines is a PCollection object. It's immutable and unordered
+        words = lines | 'Split' >> beam.FlatMap(lambda x: x.split())
+        # FlatMap is like a tuple with cardinality > 1 (unlike a list which has cardinality 1 and elements > 1)
+        counts = words | 'Count words' >> beam.combiners.Count.PerElement()
+        # Combiners are more scalable than 'group by' keys, but harder to write
+        top_words = counts | 'Find top 50' >> beam.combiners.Top.Of(50, key=lambda x:x[1])
+        top_words | beam.Map(format_output) | beam.Map(print)
+        # top_words | 'Save output' >> beam.io.WriteToText(outfile)
+
+if __name__ == '__main__':
+    main()
